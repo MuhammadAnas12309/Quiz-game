c047ebf6d81664eef9296ed0a980ddc5e3007bf1
MUHAMMAD ANAS <132845541+MuhammadAnas12309@users.noreply.github.com>
2023-05-16 18:20:31 +0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const cards = document.querySelectorAll(".memory-card");
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  this.classList.add("flip");
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+
+   console.log(firstCard.dataset.framework);
+   console.log(secondCard.dataset.framework);
+  }
+}
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
